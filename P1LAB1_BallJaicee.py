dbeb8fde3737b75c84c92c792682d68f8b6984a8
JCB-TECH <ballcadj2420@student.faytechcc.edu>
2024-09-24 14:19:29 -0400

    Add files via upload

---
+++ P1LAB1_BallJaicee.py
@@ -0,0 +1,15 @@
+#Jaicee Ball
+#9/24/2024
+#P1LAB1
+#get input from the user and display output
+
+#get first name from user
+first_name = input("Enter Your First Name: ")
+
+#get last name from user
+last_name = input("Enter Your Last Name: ")
+
+#Display info to user
+print("Hello,", first_name, last_name + "!" , "Welcome to CTI-110")
+print()
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
